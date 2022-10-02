e2652cff7bd1242624229712fbb94e6eeb5270dd
Ismael Dominguez <ismaeldb_@hotmail.com>
2022-10-02 18:23:26 -0500

    Agrega validación al servicio de notificacion

---
+++ notification/notification.py
@@ -0,0 +1,30 @@
+import datetime
+import json
+import requests
+from flask import request, Flask
+
+app = Flask(__name__)
+
+@app.route("/notification",methods =['POST'])
+def send_notification():
+    try:
+        token = request.json.get('token', None)
+
+        headers = {'content-type': 'application/json'}
+        response  = requests.post(
+            '/valid', headers=headers, data=json.dumps({
+                'token': token
+            })
+        )
+
+        if response.status_code == 200:
+            res = response.json()
+            if 'notification' in res['permissions']:
+                return {'send': True }, 200
+
+        return {'send': False }, 401
+    except:
+        return {'send': False }, 401
+
+if __name__ == "__main__":
+    app.run(debug=True, port = 5000)
